61d8093d0b880dadbd79a046f13f8dcde58c973d
Simone Smith <simonecaplin@gmail.com>
2018-12-28 10:46:46 +0000

    Add validation assertions to model

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -40,6 +40,7 @@ class Concentration {
     }
 
     func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
                 if cards[matchIndex].identifier == cards[index].identifier {
@@ -65,6 +66,7 @@ class Concentration {
     }
     
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you must have at least one pair of cards")
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
